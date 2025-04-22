fa22cf97c4606bf88674d7823d15212d12e2aa1b
Ricardo <ricardoado27@gmail.com>
2025-04-22 15:16:15 +0700

    mw pake versi ini

--- router/routeTest.py
+++ router/routeTest.py
@@ -19,6 +19,7 @@ async def testing():
   try:
     pool = await get_db() # Get The pool
 
+    # Rio
     async with pool.acquire() as conn:  # Auto Release
       async with conn.cursor() as cursor:
 
