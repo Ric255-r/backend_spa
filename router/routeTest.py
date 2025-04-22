9f6afc2241eed9c6dac700dcf837fa0318aaf2ee
Ricardo <ricardoado27@gmail.com>
2025-04-22 16:24:35 +0700

    Tambah Versi 2. routeTest.py di fungsi testing

--- router/routeTest.py
+++ router/routeTest.py
@@ -19,7 +19,7 @@ async def testing():
   try:
     pool = await get_db() # Get The pool
 
-    # Alvin suka makan pisang goreng
+    # Alvin suka makan pisang goreng. versi 2 lebih OP
     async with pool.acquire() as conn:  # Auto Release
       async with conn.cursor() as cursor:
 
