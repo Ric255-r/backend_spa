d2b855fec6985e3f69f79442123ec62363adb918
Ricardo <ricardoado27@gmail.com>
2025-04-22 15:17:48 +0700

    Tambah dikit routeTest.py

--- router/routeTest.py
+++ router/routeTest.py
@@ -19,7 +19,7 @@ async def testing():
   try:
     pool = await get_db() # Get The pool
 
-    # Rio
+    # Rio hahag
     async with pool.acquire() as conn:  # Auto Release
       async with conn.cursor() as cursor:
 
