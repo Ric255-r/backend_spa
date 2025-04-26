76efd65406bd647529343072e2342353de7e1b34
Ricardo <ricardoado27@gmail.com>
2025-04-26 18:17:12 +0700

    Tambah router transaksi

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_jwt import JwtAuthorizationCredentials
 from router.routeTest import app as app_test
 from router.user.login import app as app_login
+from router.transaksi.fnb import app as app_fnb
+from jwt_auth import access_security
 
 from koneksi import lifespan
 
@@ -20,6 +22,7 @@ main_router = APIRouter()
 
 main_router.include_router(app_test)
 main_router.include_router(app_login)
+main_router.include_router(app_fnb)
 # main_router.include_router(app_transaction)
 # main_router.include_router(app_admin)
 
@@ -32,4 +35,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="192.168.100.11", port=5500)
+  uvicorn.run(app, host="192.168.1.107", port=5500)
---
+++ router/transaksi/fnb.py
@@ -0,0 +1,72 @@
+from typing import Optional
+import uuid
+from fastapi import APIRouter, Depends, File, Form, Request, HTTPException, Security, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
+from koneksi import get_db
+from fastapi_jwt import (
+  JwtAccessBearerCookie,
+  JwtAuthorizationCredentials,
+  JwtRefreshBearer
+)
+import pandas as pd
+from aiomysql import Error as aiomysqlerror
+from jwt_auth import access_security, refresh_security, verify_jwt
+
+# Untuk Routingnya jadi http://192.xx.xx.xx:5500/api/fnb/endpointfunction
+app = APIRouter(
+  prefix="/fnb",
+  dependencies=[Depends(verify_jwt)]
+)
+
+@app.get('/lastId')
+async def getLatestTransaksi():
+  try:
+    pool = await get_db() # Get The pool
+
+    async with pool.acquire() as conn:  # Auto Release
+      async with conn.cursor() as cursor:
+
+        q1 = "SELECT id_transaksi FROM main_transaksi WHERE id_transaksi LIKE 'TF%' ORDER BY id_transaksi DESC"
+        await cursor.execute(q1)
+
+        items = await cursor.fetchone() #id transaksi terletak di index ke-0
+        id_trans = items[0] if items is not None else None
+
+        if id_trans is None:
+          num = "1"
+          strpad = "TF" + num.zfill(4)
+        else:
+          getNum = id_trans[2:]
+          num = int(getNum) + 1
+          strpad = "TF" + str(num).zfill(4)
+
+        return strpad
+
+
+  except Exception as e:
+    return JSONResponse({"Error Latest Trans": str(e)}, status_code=500)
+  
+
+@app.get('/menu')
+async def getMenu():
+  try:
+    pool = await get_db() # Get The pool
+
+    async with pool.acquire() as conn:  # Auto Release
+      async with conn.cursor() as cursor:
+        q1 = "SELECT * FROM menu_fnb"
+        await cursor.execute(q1)
+
+        items = await cursor.fetchall()
+
+        column_name = []
+        for kol in cursor.description:
+          column_name.append(kol[0])
+
+        df = pd.DataFrame(items, columns=column_name)
+        return df.to_dict('records')
+
+  except Exception as e:
+    return JSONResponse({"Error Get Menu Fnb": str(e)}, status_code=500)
+  
+  
