c80c22932e867e14eb65e807ecec7a39f16fe00a
RayXKing <alvinheng0@gmail.com>
2025-04-27 18:58:20 +0700

    tambahan file regis_kamar, regis_produk, regis_pekerja

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from router.routeTest import app as app_test
 from router.user.login import app as app_login
 from router.transaksi.fnb import app as app_fnb
 from router.admin.regis_produk import app as app_produk
+from router.admin.regis_kamar import app as app_room
+from router.admin.regis_pekerja import app as app_pekerja
 from jwt_auth import access_security
 
 from koneksi import lifespan
@@ -25,6 +27,8 @@ main_router.include_router(app_test)
 main_router.include_router(app_login)
 main_router.include_router(app_fnb)
 main_router.include_router(app_produk)
+main_router.include_router(app_room)
+main_router.include_router(app_pekerja)
 # main_router.include_router(app_transaction)
 # main_router.include_router(app_admin)
 
@@ -37,4 +41,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="10.10.10.230", port=5500)
+  uvicorn.run(app, host="192.168.1.22", port=5500)
