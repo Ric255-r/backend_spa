8cb5306af7861f62e581998566ce70fc08ca586f
RayXKing <alvinheng0@gmail.com>
2025-05-01 18:07:02 +0700

    Tambah file list_room & list_pekerja & regis_users

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from router.admin.regis_kamar import app as app_room
 from router.admin.regis_pekerja import app as app_pekerja
 from router.admin.daftarfnb import app as app_daftarfnb
 from router.admin.daftarpaketmassage import app as app_daftarpaketmassage
+from router.admin.regis_users import app as app_regisusers
+from router.admin.list_pekerja import app as app_listpekerja
+from router.admin.list_room import app as app_listroom
 # from router.transaksi.fnb import app 
 from jwt_auth import access_security
 
@@ -34,6 +37,9 @@ main_router.include_router(app_room)
 main_router.include_router(app_pekerja)
 main_router.include_router(app_daftarfnb)
 main_router.include_router(app_daftarpaketmassage)
+main_router.include_router(app_regisusers)
+main_router.include_router(app_listpekerja)
+main_router.include_router(app_listroom)
 # main_router.include_router(app_fnb)
 # main_router.include_router(app_transaction)
 # main_router.include_router(app_admin)
@@ -47,4 +53,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="192.168.1.22", port=5500)
+  uvicorn.run(app, host="10.10.10.228", port=5500)
---
+++ router/admin/list_room.py
@@ -0,0 +1,39 @@
+from typing import Optional
+import uuid
+from fastapi import APIRouter, Depends, File, Form, Request, HTTPException, Security, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
+from koneksi import get_db
+from fastapi_jwt import (
+  JwtAccessBearerCookie,
+  JwtAuthorizationCredentials,
+  JwtRefreshBearer
+)
+import pandas as pd
+from aiomysql import Error as aiomysqlerror
+
+app = APIRouter(
+  prefix="/listroom",
+)
+
+@app.get('/dataroom')
+async def getDataRoom():
+  try:
+    pool = await get_db() # Get The pool
+
+    async with pool.acquire() as conn:  # Auto Release
+      async with conn.cursor() as cursor:
+        await cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;")
+        q1 = "SELECT * FROM ruangan ORDER BY nama_ruangan ASC"
+        await cursor.execute(q1)
+
+        items = await cursor.fetchall()
+
+        column_name = []
+        for kol in cursor.description:
+          column_name.append(kol[0])
+
+        df = pd.DataFrame(items, columns=column_name)
+        return df.to_dict('records')
+
+  except Exception as e:
+    return JSONResponse({"Error Get Data Ruangan": str(e)}, status_code=500)
