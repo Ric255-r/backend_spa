1564c3f611d754ff25f9d8c293c6c25b91f4510c
RayXKing <alvinheng0@gmail.com>
2025-05-09 16:30:23 +0700

    tambah list_transaksi

--- router/admin/list_transaksi.py
+++ router/admin/list_transaksi.py
@@ -46,32 +46,47 @@ async def getDataDetailTrans():
     async with pool.acquire() as conn:  # Auto Release
       async with conn.cursor() as cursor:
         await cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;")
-        q1 = """SELECT 
-            dt.id_detail_transaksi,
-            GROUP_CONCAT(DISTINCT mt.id_produk) AS produk_list,
-            GROUP_CONCAT(DISTINCT mt.id_fnb) AS fnb_list,
-            GROUP_CONCAT(DISTINCT mt.id_paket) AS paket_list,
-  
-            GROUP_CONCAT(DISTINCT dtp.id_produk) AS produk_list_detail,
-            GROUP_CONCAT(DISTINCT dtp.qty) AS produk_qty_list,
-            GROUP_CONCAT(DISTINCT dtp.satuan) AS produk_satuan_list,
-            GROUP_CONCAT(DISTINCT dtp.harga_item) AS produk_harga_item_list,
-            GROUP_CONCAT(DISTINCT dtp.harga_total) AS produk_harga_total_list,
+        q1 = """
+             SELECT 
+              mt.id_transaksi,
+              mt.id_detail_transaksi,
 
-            GROUP_CONCAT(DISTINCT dtpk.id_paket) AS paket_list_detail,
-            GROUP_CONCAT(DISTINCT dtpk.qty) AS paket_qty_list,
-            GROUP_CONCAT(DISTINCT dtpk.satuan) AS paket_satuan_list,
-            GROUP_CONCAT(DISTINCT dtpk.durasi_awal) AS paket_durasi_awal_list,
-            GROUP_CONCAT(DISTINCT dtpk.total_durasi) AS paket_total_durasi_list,
-            GROUP_CONCAT(DISTINCT dtpk.harga_item) AS paket_harga_item_list,
-            GROUP_CONCAT(DISTINCT dtpk.harga_total) AS paket_harga_total_list
+              GROUP_CONCAT(DISTINCT dtp.id_produk) AS produk_list,
 
-            FROM detail_transaksi dt
-            LEFT JOIN main_transaksi mt ON dt.id_detail_transaksi = mt.id_detail_transaksi
-            LEFT JOIN detail_transaksi_produk dtp ON dt.id_detail_transaksi = dtp.id_detail_transaksi
-            LEFT JOIN detail_transaksi_paket dtpk ON dt.id_detail_transaksi = dtpk.id_detail_transaksi
-            GROUP BY dt.id_detail_transaksi"""
+              GROUP_CONCAT(DISTINCT dt.id_fnb) AS fnb_list,
+
+              GROUP_CONCAT(DISTINCT dtpk.id_paket) AS paket_list,
+
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.id_produk, '')) AS produk_list_detail,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.qty, 0)) AS produk_qty_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.satuan, '')) AS produk_satuan_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.harga_item, 0)) AS produk_harga_item_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.harga_total, 0)) AS produk_harga_total_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.durasi_awal, 0)) AS produk_durasi_awal_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtp.total_durasi, 0)) AS produk_total_durasi_list,
+
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.id_paket, '')) AS paket_list_detail,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.qty, 0)) AS paket_qty_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.satuan, '')) AS paket_satuan_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.durasi_awal, 0)) AS paket_durasi_awal_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.total_durasi, 0)) AS paket_total_durasi_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.harga_item, 0)) AS paket_harga_item_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dtpk.harga_total, 0)) AS paket_harga_total_list,
+
+              GROUP_CONCAT(DISTINCT COALESCE(dt.qty, 0)) AS fnb_qty_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dt.satuan, '')) AS fnb_satuan_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dt.harga_item, 0)) AS fnb_harga_item_list,
+              GROUP_CONCAT(DISTINCT COALESCE(dt.harga_total, 0)) AS fnb_harga_total_list
+
+            FROM main_transaksi mt
+            LEFT JOIN detail_transaksi dt ON mt.id_detail_transaksi = dt.id_detail_transaksi
+            LEFT JOIN detail_transaksi_produk dtp ON mt.id_detail_transaksi = dtp.id_detail_transaksi
+            LEFT JOIN detail_transaksi_paket dtpk ON mt.id_detail_transaksi = dtpk.id_detail_transaksi
+            GROUP BY mt.id_transaksi, mt.id_detail_transaksi;
+              """
+        await cursor.execute("SET SESSION group_concat_max_len = 100000;")
         await cursor.execute(q1)
+        
 
         items = await cursor.fetchall()
 
@@ -83,7 +98,7 @@ async def getDataDetailTrans():
         return df.to_dict('records')
 
   except Exception as e:
-    return JSONResponse({"Error Get Data Ruangan": str(e)}, status_code=500)
+    return JSONResponse({"Error Get Data Detail Trans": str(e)}, status_code=500)
 
 
 
