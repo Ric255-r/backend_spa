02ec08b4127b179e8ba0e0c6268a25a25a4e752b
RayXKing <alvinheng0@gmail.com>
2025-05-16 10:09:12 +0700

    config lama

--- main.py
+++ main.py
@@ -101,4 +101,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="192.168.21.165", port=5500)
+  uvicorn.run(app, host="192.168.1.17", port=5500)
