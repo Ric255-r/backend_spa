2d16acf57fb1b3be492a1674f5eb473208f37b0d
Ricardo <ricardoado27@gmail.com>
2025-05-20 14:45:52 +0700

    tambah ganti ruangan di revisi_data.py

--- main.py
+++ main.py
@@ -111,4 +111,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="192.168.31.183", port=5500)
+  uvicorn.run(app, host="192.168.84.166", port=5500)
