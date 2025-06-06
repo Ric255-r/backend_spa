14d2f2448703498764ec53464bca0b59cd721b32
Ricardo <ricardoado27@gmail.com>
2025-06-06 10:15:17 +0700

    pull pny rio

--- main.py
+++ main.py
@@ -133,4 +133,4 @@ if __name__ == "__main__":
   import uvicorn
   # Cara jalanin dgn Reload
   # uvicorn main:app --reload --host 192.168.100.11 --port 5500
-  uvicorn.run(app, host="172.16.0.233", port=5500)
+  uvicorn.run(app, host="192.168.84.166", port=5500)
